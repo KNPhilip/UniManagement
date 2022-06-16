2d60acc247ccd07c0d4b73694d9b7d77ceefc0ac
phil5520 <phil5520@CV-BB-7592>
2022-06-16 14:39:36 +0200

    Added repo test

--- Tests/EntitiesTests.cs
+++ Tests/EntitiesTests.cs
@@ -36,14 +36,12 @@ namespace Tests
 
             //Act
             ContactInformation contactInformation = new(id, mail, phoneNumber);
-            int kid = 1;
-            string kmail = "philip@gmail.com";
-            string kphoneNumber = "53253213";
+            contactInformation.Mail = "trumpfanclub@gmail.com";
+            contactInformation.PhoneNumber = "95832021";
 
             //Assert
-            Assert.True(!(contactInformation.Id == kid));
-            Assert.True(!(contactInformation.Mail == kmail));
-            Assert.True(!(contactInformation.PhoneNumber == kphoneNumber));
+            Assert.Equal("trumpfanclub@gmail.com", contactInformation.Mail);
+            Assert.Equal("95832021", contactInformation.PhoneNumber);
 
         }
     }
---
+++ Tests/RepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using Entities;
+using Xunit;
+
+namespace Tests
+{
+    public class RepositoryTests
+    {
+        [Fact]
+        public void GetRepositoryDataTest()
+        {
+            //Arrange
+            Repository repository;
+
+            //Act
+            repository = new();
+
+            //Assert
+            Assert.NotNull(repository);
+        }
+        [Fact]
+        public void GetAllPersonsAndTestIfDataReturns()
+        {
+            //Arrange
+            Repository repo = new();
+
+            //Act
+            List<Person> people = repo.GetAllPersons();
+
+            //Assert
+            Assert.True(people.Count > 0);
+        }
+    }
+}
